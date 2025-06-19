8cb7564f8647f69dd6d0ec180ba46c10d837d21e
Alex <zavulon82@gmail.com>
2025-06-19 12:34:33 +0400

    first exercise

--- js/script4.js
+++ js/script4.js
@@ -21,3 +21,18 @@ console.log(++incr);
 console.log(--decr);
 
 console.log(5 % 2); // остаток от деления
+console.log(2 * 4 == 8); // сравнение
+console.log(2 * 4 === '8'); // строгое сравнение
+
+// && логическое И
+// || логическое ИЛИ
+// ! логическое НЕ
+
+const isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose); // логическое ИЛИ
+// console.log(isChecked && isClose); // логическое И   
+
+console.log(isChecked && !isClose); // логическое И с отрицанием
+// console.log(isChecked || !isClose); // логическое ИЛИ с отрицанием
